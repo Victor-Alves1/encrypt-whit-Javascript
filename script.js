d1db0957f668059c38cc25f58451aff4fa1084b0
Victor Alves da Costa <victoralves741@gmail.com>
2022-08-05 09:43:46 -0300

    Descriptografia completa

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ function encrypt(){
     //Função para mudar os caracteres
     function crypt (str, disp){
         for(let i = 0; i<str.length; i++){
-            if(disp == '-1'){
-                str[i] = (str[i].charCodeAt()) + (parseInt(disp));
-                cryptoCode[i] = (String.fromCharCode(str[i]));
-            } else {
-                if(str[i].charCodeAt()>=65 && str[i].charCodeAt()<=90 || str[i].charCodeAt()>=97 && str[i].charCodeAt()<=122){
-                    str[i] = (str[i].charCodeAt()) + (parseInt(disp));
-                    cryptoCode[i] = (String.fromCharCode(str[i])); 
-                }
-            }
+            if(str[i].charCodeAt()>=65 && str[i].charCodeAt()<=90 || str[i].charCodeAt()>=97 && str[i].charCodeAt()<=122){
+                str[i] = (str[i].charCodeAt()) + (parseInt(disp)) - 1
+                cryptoCode[i] = str[i]
+            } else { 
+                str[i] = (str[i].charCodeAt()) + (-1)
+                cryptoCode[i] = str[i]
+            };
+            cryptoCode[i] = (cryptoCode[i]>>>0).toString(2)
         }
     }
 
@@ -21,27 +20,27 @@ function encrypt(){
         return (cryptoCode = joinString);
     }
 
-    // var input = require('fs').readFileSync('./dev/stdin', 'utf8');
-    // var inputNum = 2
-    var input = window.document.querySelector('#mensagem').value
-    var inputNum = window.document.querySelector('#chave').value
-    var res = window.document.querySelector('#res')
+    // Captura das informações no HTML
+    var input = window.document.querySelector('#mensagem').value;
+    var inputNum = window.document.querySelector('#chave').value;
+    var res = window.document.querySelector('#res');
     var parametros = input.split('\r\n');
-    // Valor do primeiro deslocamento
-    // var inputNum = 10
+
+    //Validação
+    if(inputNum == 0 || inputNum == ''){
+        window.alert('Escolha um valor chave para realizar a criptografia/descriptografia')
+    };
+
+    //Criptografar todos os caracteres
     for(let h = 0; h< parametros.length; h++){
         var cryptoCode = parametros.shift();
         cryptoCode = cryptoCode.split('');
-        //First Step
+        //First Step e Third Step
         crypt(cryptoCode, inputNum);
         //Second Step
         reverseString(cryptoCode);
-        //Third Step
-        crypt(cryptoCode, '-1');
         //Grouping all
-        console.log(cryptoCode);
-        cryptoCode=cryptoCode.join('');
-        console.log(cryptoCode);
+        cryptoCode=cryptoCode.join(' ');
         res.innerHTML += `<p> ${cryptoCode} </p>`
     }
 };
@@ -50,17 +49,16 @@ function encrypt(){
 function decrypt(){
     //Função para mudar os caracteres    
     function crypt (str, disp){
-        for(let i = 0; i<str.length; i++){
-            if(disp == 'uno'){
-                str[i] = (str[i].charCodeAt()) + (1);
-                cryptoCode[i] = (String.fromCharCode(str[i]));
+            str = parseInt(str, 2)
+            console.log(str)
+            if(str>=65 && str<=90 || str>=97 && str<=122){
+                str = (str) - (parseInt(disp)) + 1;
+                str = (String.fromCharCode(str)); 
             } else {
-                if(str[i].charCodeAt()>=65 && str[i].charCodeAt()<=90 || str[i].charCodeAt()>=97 && str[i].charCodeAt()<=122){
-                    str[i] = (str[i].charCodeAt()) - (parseInt(disp));
-                    cryptoCode[i] = (String.fromCharCode(str[i])); 
-                }
+                str = (str) + (1);
+                str = (String.fromCharCode(str));
             }
-        }
+            cryptArr.push(str);
     }
 
     //Função para usar a string invertida
@@ -69,27 +67,23 @@ function decrypt(){
         return (cryptoCode = joinString);
     }
 
-    // var input = require('fs').readFileSync('./dev/stdin', 'utf8');
-    // var inputNum = 2
-    var input = window.document.querySelector('#mensagem').value
-    var inputNum = window.document.querySelector('#chave').value
-    var res = window.document.querySelector('#res')
-    var parametros = input.split('\r\n');
-    // Valor do primeiro deslocamento
-    // var inputNum = 10
-    for(let h = 0; h< parametros.length; h++){
+    // Captura das informações no HTML    
+    var input = window.document.querySelector('#mensagem').value;
+    var inputNum = window.document.querySelector('#chave').value;
+    var res = window.document.querySelector('#res');
+    var parametros = input.split(' ');
+    var totalLength = parametros.length;
+    var cryptArr = [];
+
+    //Criptografar todos os caracteres
+    for(let h = 0; h<= totalLength; h++){
         var cryptoCode = parametros.shift();
-        cryptoCode = cryptoCode.split('');
-        //First Step
+        //First Step and Third Step
         crypt(cryptoCode, inputNum);
-        //Second Step
-        reverseString(cryptoCode);
-        //Third Step
-        crypt(cryptoCode, 'uno');
-        //Grouping all
-        console.log(cryptoCode);
-        cryptoCode=cryptoCode.join('');
-        console.log(cryptoCode);
-        res.innerHTML += `<p> ${cryptoCode} </p>`
     }
+    //Second Step
+    reverseString(cryptArr);
+    //Grouping all
+    cryptoCode=cryptArr.join('');
+    res.innerHTML += `<p> ${cryptoCode} </p>`
 };
